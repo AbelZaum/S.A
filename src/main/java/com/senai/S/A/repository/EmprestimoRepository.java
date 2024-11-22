fb5245e21887f0210515fdccb78f4fe06b4265b2
Gabriel Abel da Silva <127804266+AbelZaum@users.noreply.github.com>
2024-11-21 21:31:15 -0300

    Delete src directory

--- src/main/java/com/senai/S/A/repository/EmprestimoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.S.A.repository;
-
-import com.senai.S.A.model.Emprestimo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long> {
-}
